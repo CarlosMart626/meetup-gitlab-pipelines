bd66528e9b854a9896ea4b6c3624536b7c5515d7
Carlos Martinez <carlosmart626@gmail.com>
2018-03-22 08:04:24 -0500

    Update integration tests

--- tests/integration_tests.py
+++ tests/integration_tests.py
@@ -2,5 +2,10 @@ import requests
 
 
 def test_admin_url():
-    r = requests.get('https://api.github.com/events')
+    r = requests.get('https://meetup.carlosmart.co/admin/')
     assert r.status_code == 200
+
+
+def test_root_url():
+    r = requests.get('https://meetup.carlosmart.co/')
+    assert r.status_code == 404
